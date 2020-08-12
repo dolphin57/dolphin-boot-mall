3e11b870cbf9b02709ca3f00d19aa5c05258f7d4
120444121 <ericql57@163.com>
2020-08-12 11:34:19 +0800

    项目初始化

---
+++ src/main/java/io/dolphin/mall/DolphinBootMallApplication.java
@@ -0,0 +1,13 @@
+package io.dolphin.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolphinBootMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DolphinBootMallApplication.class, args);
+    }
+
+}
